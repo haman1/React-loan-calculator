236d18c4fce87f8d64b048d7f47a6f6ce152569c
ombasa <ombasaherman@gmail.com>
2022-04-03 13:21:29 +0300

    src

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './normalize.css';
+import './skeleton.css';
+import Form from './components/Form'
+function App() {
+  return (
+    <div>
+      <h1>Loan Calculator</h1>
+      <div className="container">
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
